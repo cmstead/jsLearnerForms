55974602b95e350c4ffbda28031e1aa1fd27cc1e
Chris Stead <cmstead@gmail.com>
2017-06-16 22:35:02 -0700

    Migrated to quokka approvals helper

--- test/helpers/approvalsConfig.js
+++ test/helpers/approvalsConfig.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const approvalsLocation = './test/approvals';
-const quokkaApprovalsHelper = require('./quokkaApprovalsHelper')();
+const quokkaApprovalsHelper = require('quokka-mocha-approvals-helper')();
 const approvalsConfigFactory = require('approvals-config-factory');
 
 const approvalsConfig = approvalsConfigFactory.buildApprovalsConfig({ 
-    reporter: quokkaApprovalsHelper.chooseReporter('kdiff3')
+    reporter: chooseReporter('kdiff3')
 });
 
 module.exports = require('approvals').configure(approvalsConfig).mocha(approvalsLocation);
