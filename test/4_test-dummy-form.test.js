25d89ff0ecbcfe4b06bf1ced16031fcf7eca8018
Chris Stead <cmstead@gmail.com>
2017-06-23 21:01:54 -0700

    Rolled back to start of test-dummy form

--- test/4_test-dummy-form.test.js
+++ test/4_test-dummy-form.test.js
@@ -30,63 +30,35 @@ describe('Test Dummy Form', function () {
 
         // Checking if two values are equal can be done with assert.equal.
         // Assert is part of the Chai testing package.
-        it('should return 0 when two values are equal', function () {
-            let result = testDummyForm.compareNames('name', 'name');
-            assert.equal(result, 0);
-        });
+        it('should return 0 when two values are equal');
 
         // Equality checking is one of the most common ways to test
         // the results of code, so it's important to get comnfortable with it
-        it('should return -1 when name1 comes before name2 alphabetically', function () {
-            let result = testDummyForm.compareNames('Joe', 'Sally');
-            assert.equal(result, -1);
-        });
+        it('should return -1 when name1 comes before name2 alphabetically');
 
         // It's important to test all cases. The function, compareNames, has
         // three distinct outcomes. One of the core ideas in testing your code
         // is to test until you're bored. Ideally, being bored will come at
         // about the same time you've tested all cases.
-        it('should return 1 when name1 comes after name2 alphabetically', function () {
-            let result = testDummyForm.compareNames('Sally', 'Joe');
-            assert.equal(result, 1);
-        });
+        it('should return 1 when name1 comes after name2 alphabetically');
 
     });
 
     describe('compareContactRecords', function () {
 
-        let record1;
-        let record2;
-
-        beforeEach(function () {
-            record1 = { firstName: 'Joe', lastName: 'Smith' };
-            record2 = { firstName: 'Sally', lastName: 'Jensen' };
-        });
-
         // Typically it's not necessary to test an extracted function, but it is useful to see
         // tests written in small steps at times. Let's write some tests like we would
         // normally do.
-        it('should return 0 when first and last name are equal between two records', function () {
-            let result = testDummyForm.compareContactRecords(record1, record1);
-            assert.equal(result, 0);
-        });
+        it('should return 0 when first and last name are equal between two records');
 
         // Let's just get the next couple tests out of the way. They're like the first ones we wrote,
         // but comparing records.
-        it('should return -1 when record1.lastName comes before record2.lastName alphabetically', function () {
-            let result = testDummyForm.compareContactRecords(record2, record1);
-
-            assert.equal(result, -1);
-        });
+        it('should return -1 when record1.lastName comes before record2.lastName alphabetically');
 
         // After completing this test, we will have repeated code. Let's do this in two parts:
         // - complete the test 
         // - lift the duplicated records into a beforeEach block
-        it('should return 1 when record1.lastName comes after record2.lastName alphabetically', function () {
-            let result = testDummyForm.compareContactRecords(record1, record2);
-
-            assert.equal(result, 1);
-        });
+        it('should return 1 when record1.lastName comes after record2.lastName alphabetically');
 
         it('should return -1 when last names are equal and record1.firstName comes before record2.firstName alphabetically');
         it('should return 1 when last names are equal and record1.firstName comes after record2.firstName alphabetically');
