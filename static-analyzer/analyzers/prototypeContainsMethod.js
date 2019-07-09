7b75cda402028ad9f205846a7a67df7ee07d3b0c
Chris Stead <cmstead@gmail.com>
2019-07-09 15:55:59 -0700

    Updated form 2, completing all refactor steps

--- static-analyzer/analyzers/prototypeContainsMethod.js
+++ static-analyzer/analyzers/prototypeContainsMethod.js
@@ -1,7 +1,33 @@
-function prototypeContainsMethod() {
-    
-    function isMethodOnPrototype() {
-        throw new Error('This needs to be written');
+function prototypeContainsMethod({
+    objectName = null,
+    methodName = null
+}) {
+
+    function isPrototypeObjectAssignment(node) {
+        return node.type === 'AssignmentExpression'
+            && node.left.type === 'MemberExpression'
+            && node.left.object.name === objectName
+            && node.left.property.name === 'prototype'
+            && node.right.type === 'ObjectExpression';
+    }
+
+    function doesObjectContainMethod(node) {
+        let methodWasFound = false;
+
+        node.properties.forEach(function (property) {
+            methodWasFound = methodWasFound
+                || (
+                    property.key.name === methodName
+                    && property.value.type === 'FunctionExpression'
+                );
+        });
+
+        return methodWasFound;
+    }
+
+    function isMethodOnPrototype(node) {
+        return isPrototypeObjectAssignment(node)
+            && doesObjectContainMethod(node.right);
     }
 
     return {
