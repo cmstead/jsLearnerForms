95666362f3ed2362804f981d6436f93099f67b33
Chris Stead <cmstead@gmail.com>
2019-07-12 15:35:06 -0700

    Reset form 1

--- jsforms-source/1_first-form.js
+++ jsforms-source/1_first-form.js
@@ -1,13 +1,6 @@
 const jsforms = (function () {
     'use strict';
 
-    var a = 5;
-    a = 3 + 7;
-    
-    const b = 'Hello, World!';
-    console.log(b);
-
     return {
-        b
     };
 })();
